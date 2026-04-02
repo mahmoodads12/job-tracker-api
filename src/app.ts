fa430943fbbc2d9a9869687bb532f779bdae94bc
mahmoodads12 <mahmoodads12@gmail.com>
2026-04-03 02:40:20 +0300

    fix: resolve module resolution issue with routes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
 import express from 'express';
-
+import router from './routes';
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-
+app.use('/', router);
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
