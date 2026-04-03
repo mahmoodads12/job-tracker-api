41407afeddf971c068dc9adc9fe72213e87a6944
mahmoodads12 <mahmoodads12@gmail.com>
2026-04-03 12:01:50 +0300

    fix: prisma module not found

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import router from './routes';
+import { prisma } from './prisma';
+
 const app = express();
 
 app.use(express.json());
+
+app.get('/test-db', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
 
 app.use('/', router);
 app.listen(3000, () => {
